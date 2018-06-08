594b7cd2cad900e04613a1a75ec3fb729af9c78b
Raul Herranz Munoz <raul.herranz@scmspain.com>
2018-06-08 13:31:48 +0200

    Adding sample form

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css'
 // import App from './App'
 // import App from './AppConditional'
 //import App from './AppObjectsList'
-import App from './AppEventsClick'
+//import App from './AppEventsClick'
+import App from './AppForms'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
