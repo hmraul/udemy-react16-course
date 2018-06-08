5ab45f1ae274056e893277caab6bc14cfaf97348
Raul Herranz Munoz <raul.herranz@scmspain.com>
2018-06-08 11:00:44 +0200

    Simple list case

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-import App from './AppConditional';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+// import App from './App'
+// import App from './AppConditional'
+import App from './AppList'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
