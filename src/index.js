137612efea42139784550a1598d3fd20997fa8f7
Raul Herranz Munoz <raul.herranz@scmspain.com>
2018-06-08 13:14:59 +0200

    Adding events lesson

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom'
 import './index.css'
 // import App from './App'
 // import App from './AppConditional'
-import App from './AppObjectsList'
+//import App from './AppObjectsList'
+import App from './AppEventsClick'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
