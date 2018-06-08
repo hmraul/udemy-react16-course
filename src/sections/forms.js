84cd479bec43be489cf59fd7d46a3d357aaa7b7e
Raul Herranz Munoz <raul.herranz@scmspain.com>
2018-06-08 13:55:03 +0200

    Componentes controlados

--- src/sections/forms.js
+++ src/sections/forms.js
@@ -1,16 +1,27 @@
 import React, {Component} from 'react'
 
 export default class Forms extends Component {
+  constructor() {
+    super()
+    this.state = {
+      inputName: '',
+      inputTwitter: '@',
+      inputTerms: true
+    }
+  }
+
   handleSubmit = (e) => {
     e.preventDefault()
-    const name = this.inputName.value
-    const twitter = document.getElementById('twitter').value
-    console.log(name, twitter)
+    // const name = this.inputName.value
+    // const twitter = document.getElementById('twitter').value
+    // console.log(name, twitter)
+    console.log(this.state)
   }
 
   handleChange = (e) => {
     console.log('handleChange')
     console.log(e.target.checked)
+    this.setState({inputTerms: e.target.checked })
   }
 
   render () {
@@ -23,19 +34,26 @@ export default class Forms extends Component {
             <input
               id='name'
               name='userName'
+              onChange={e => this.setState({inputName: e.target.value})}
               placeholder='Introduce tu nombre' 
-              ref={inputElement => this.inputName = inputElement}/>
+              ref={inputElement => this.inputName = inputElement}
+              value={this.state.inputName} />
           </p>
           <p>
             <label>Twitter: </label>
             <input
               id='twitter'
               name='twitterAccount'
-              placeholder='Introduce tu twitter' />
+              onChange={e => this.setState({inputTwitter: e.target.value})}
+              placeholder='Introduce tu twitter' 
+              value={this.state.inputTwitter} />
           </p>
           <p>
             <label>
-              <input onChange={this.handleChange} type='checkbox' />
+              <input 
+                onChange={this.handleChange} 
+                checked={this.state.inputTerms} 
+                type='checkbox' />
               Accepted terms
             </label>
           </p>
